0580b0c968fcffe02dfbc0bf0580c13f0a3e2b76
Alex <alex.safonovaa@yandex.ru>
2024-09-30 14:15:56 +0300

    Add files via upload

---
+++ tests_12_4.py
@@ -0,0 +1,65 @@
+import logging
+import unittest
+logging.basicConfig(level=logging.INFO, filename='runner_tests.log', filemode='w', encoding='UTF-8', format='%(levelname)s: %(message)s')
+class Runner:
+    def __init__(self, name, speed=5):
+        if isinstance(name, str):
+            self.name = name
+        else:
+            raise TypeError(f'Имя может быть только строкой, передано {type(name).name}')
+        self.distance = 0
+        if speed > 0:
+            self.speed = speed
+        else:
+            raise ValueError(f'Скорость не может быть отрицательной, сейчас {speed}')
+
+    def run(self):
+        self.distance += self.speed * 2
+
+    def walk(self):
+        self.distance += self.speed
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, Runner):
+            return self.name == other.name
+
+class Tournament:
+    def __init__(self, distance, *participants):
+        self.full_distance = distance
+        self.participants = list(participants)
+
+    def start(self):
+        finishers = {}
+        place = 1
+        while self.participants:
+            for participant in self.participants:
+                participant.run()
+                if participant.distance >= self.full_distance:
+                    finishers[place] = participant
+                    place += 1
+                    self.participants.remove(participant)
+
+        return finishers
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        try:
+            runner = Runner("Иван", -1)
+            logging.info('"test_walk" выполнен успешно')
+        except ValueError as e:
+            logging.warning(f"Неверная скорость для Runner: {e}")
+
+    def test_run(self):
+        try:
+            runner = Runner(123)
+            logging.info('"test_run" выполнен успешно')
+        except TypeError as e:
+            logging.warning(f"Неверный тип данных для объекта Runner: {e}")
